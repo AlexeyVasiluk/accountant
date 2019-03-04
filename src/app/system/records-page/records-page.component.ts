7dbe57e6221a02470d0cce51dab7fff9f4bee45f
Oleksii Vasyliuk <olvas@smile.fr>
2019-03-04 18:22:25 +0200

    functional edit category added

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoriesService} from '../shared/services/categories.service';
+import {Category} from '../shared/models/category.model';
 
 @Component({
   selector: 'av-records-page',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordsPageComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  isLoaded = false;
+
+  constructor(private categoriesService: CategoriesService) {
+  }
 
   ngOnInit() {
+    this.categoriesService.getCategories()
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+        this.isLoaded = true;
+      });
+  }
+
+  newCategoryAdded(category: Category) {
+    this.categories.push(category);
+  }
+
+  categoryWasEdited(category: Category) {
+    const idx = this.categories
+      .findIndex(c => c.id === category.id);
+    this.categories[idx] = category;
   }
 
 }
